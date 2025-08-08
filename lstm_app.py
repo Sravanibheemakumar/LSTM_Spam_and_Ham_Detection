ebf152ab8d421a2c579ccbb1aa4e2c8a8cf09c3b
Sravani <bheemakumarsravanip@gmail.com>
2025-08-08 21:31:49 +0530

    Update lstm_app.py

--- lstm_app.py
+++ lstm_app.py
@@ -1,37 +1,3 @@
-# import streamlit as st
-# import numpy as np
-# from keras.models import load_model
-# from keras.preprocessing.sequence import pad_sequences
-# import pickle
-
-# # Load tokenizer
-# with open("tokenizer.pkl", "rb") as f:
-#     tokenizer = pickle.load(f)
-
-# # Load model
-# model = load_model("spam_lstm_model.keras", compile=False)
-
-# # Streamlit UI
-# st.title("📩 Spam vs Ham Detector (LSTM)")
-# message = st.text_area("Enter your message:")
-
-# if st.button("Predict"):
-#     if message.strip() == "":
-#         st.warning("Please enter a message.")
-#     else:
-#         # Preprocess input
-#         seq = tokenizer.texts_to_sequences([message])
-#         padded = pad_sequences(seq, maxlen=100)
-
-#         # Predict
-#         pred = model.predict(padded)[0][0]
-
-#         if pred > 0.5:
-#             st.error("🚨 Spam Message Detected!")
-#         else:
-#             st.success("✅ It's a Ham (Not Spam) Message.")
-
-
 import streamlit as st
 import numpy as np
 from keras.models import load_model
@@ -75,6 +41,46 @@ if st.button("Predict"):
             else:
                 st.success("✅ It's a Ham (Not Spam) Message.")
 
+
+
+
+# import streamlit as st
+# import numpy as np
+# from keras.models import load_model
+# from keras.preprocessing.sequence import pad_sequences
+# import pickle
+
+# # Load tokenizer
+# with open("tokenizer.pkl", "rb") as f:
+#     tokenizer = pickle.load(f)
+
+# # Load model
+# model = load_model("spam_lstm_model.keras", compile=False)
+
+# # Streamlit UI
+# st.title("📩 Spam vs Ham Detector (LSTM)")
+# message = st.text_area("Enter your message:")
+
+# if st.button("Predict"):
+#     if message.strip() == "":
+#         st.warning("Please enter a message.")
+#     else:
+#         # Preprocess input
+#         seq = tokenizer.texts_to_sequences([message])
+#         padded = pad_sequences(seq, maxlen=100)
+
+#         # Predict
+#         pred = model.predict(padded)[0][0]
+
+#         if pred > 0.5:
+#             st.error("🚨 Spam Message Detected!")
+#         else:
+#             st.success("✅ It's a Ham (Not Spam) Message.")
+
+
+
+
  
+
 
 
